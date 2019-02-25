a4b44a85850e5c2e28d9ea857aec8c3835eea461
Arsh Zahed <arshzahed@gmail.com>
2019-02-24 20:03:04 -0800

    Cleaned curiosity

--- curiosity/curiosity_net.py
+++ curiosity/curiosity_net.py
@@ -2,9 +2,22 @@ import tensorflow as tf
 import numpy as np
 import os
 
-class Net:
+class CuriosityNet:
+	"""Basic Curiosity Module with Random Network Distillation
+
+	TODO:
+		Modularize it to take any network structure and build it
+		for both the fixed and trainable networks.
+
+	@Authors:
+		An Wang
+		Arsh Zahed
+	"""
 
 	def __init__(self):
+		"""
+		Builds and initializes parameters	
+		"""
 		self.input_size = 10
 		self.output_size = 10
 		self.batch_size = 1
@@ -13,38 +26,65 @@ class Net:
 		self.sess.run(tf.global_variables_initializer())
 
 	def _build_model(self):
+		"""
+		Builds the graph for the curiosity module.
+		"""
 		self.x = tf.placeholder(tf.float32, [self.batch_size, self.input_size])
 
 		# Model 1: Fixed network
 		self.num_units1 = 128
 		with tf.variable_scope("fixed"):
-			self.m1_l1 = tf.layers.dense(self.x, self.num_units1, activation=tf.nn.relu, trainable=False)
-			self.truth = tf.layers.dense(self.m1_l1, self.output_size, activation=tf.nn.relu, trainable=False)
+			self.m1_l1 = tf.layers.dense(self.x, 
+				                         self.num_units1, 
+										 activation=tf.nn.relu, 
+										 trainable=False)
+			self.truth = tf.layers.dense(self.m1_l1, 
+				     					 self.output_size, 
+				     					 activation=tf.nn.relu, 
+				     					 trainable=False)
 
-		self.weights = tf.get_default_graph().get_tensor_by_name(os.path.split(self.m1_l1.name)[0] + '/kernel:0')
+		self.weights = tf.get_default_graph().get_tensor_by_name(os.path.split(
+										 self.m1_l1.name)[0] + '/kernel:0')
 
 		# Model 2: Prediction network
 		self.num_units2 = 128
 		with tf.variable_scope("trainable"):
-			self.m2_l1 = tf.layers.dense(self.x, self.num_units2, activation=tf.nn.relu)
-			self.pred = tf.layers.dense(self.m2_l1, self.output_size, activation=tf.nn.relu)
+			self.m2_l1 = tf.layers.dense(self.x, 
+										 self.num_units2, 
+										 activation=tf.nn.relu)
+			self.pred = tf.layers.dense(self.m2_l1, 
+										self.output_size, 
+										activation=tf.nn.relu)
 
-		self.losses = tf.losses.mean_squared_error(labels=self.truth, predictions=self.pred)
+		self.losses = tf.losses.mean_squared_error(labels=self.truth, 
+												   predictions=self.pred)
 		self.total_loss = tf.reduce_mean(self.losses)
-		self.optimizer = tf.train.RMSPropOptimizer(tf.constant(0.005),0.995).minimize(self.total_loss)
+		self.optimizer = tf.train.RMSPropOptimizer(tf.constant(0.005),
+												   0.995).minimize(self.total_loss)
 
 	def train(self, x):
-		# print(tf.trainable_variables())
-		# last_weights = None
+		"""
+		Train Loop, NOT FINAL!!!!!!
+		"""
 		while True:
-			loss, weights, _ = self.sess.run([self.total_loss, self.weights, self.optimizer], feed_dict={self.x: x})
-			# print("loss: %f", loss)
-			# print("weights: ", weights == last_weights)
-			# last_weights = weights
+			loss = self.step(x)
+			print("loss: %f", loss)
 
 	def step(self, x):
-		loss, _ = self.sess.run([self.total_loss, self.optimizer], feed_dict={self.x: x})
+		"""
+		Does a single optimization step
 
-# net = Net()
-# test = np.array([np.random.rand(net.input_size)])
-# net.train(test)
+		Args:
+			x: Array input to the curiosity module
+		"""
+		loss, _ = self.sess.run([self.total_loss, 
+								 self.optimizer], 
+								feed_dict={self.x: x})
+		return loss
+
+
+if __name__ == "__main__":
+	net = CuriosityNet()
+	test = np.array([np.random.rand(net.input_size)])
+	net.train(test)
+
