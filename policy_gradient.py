9e1538af475b537aeb9e88f61dead136248a0e71
Praveen Batra <praveen.batra@berkeley.edu>
2019-04-06 16:11:21 -0700

    Switch to dictionary based rollout

--- policy_gradient.py
+++ policy_gradient.py
@@ -2,83 +2,63 @@ import tensorflow as tf
 import numpy as np
 import gym
 import architectures as arch
-
+import time
 
 class Rollouts:
-    """
-    Rollouts contains the following arrays
-    - states: States from state_0 ... state_n
-    - actions: Actions from action_0 ... action_n
-    - rewards: Rewards from reward_0 ... reward_n
-    - next_states: States from state_1 ... state_n+1 (terminal state)
-    """
-
-    def __init__(self, discount_factor):
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.next_states = []
-        self.cumulative_rewards = []
-        self.discount_factor = discount_factor
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.steps = []
 
     def push(self, state, action, reward, next_state):
-        self.states.append(state)
-        self.actions.append(action)
-        self.rewards.append(reward)
-        self.next_states.append(next_state)
+        self.steps.append({
+            'state': state,
+            'action': action,
+            'reward': reward,
+            'next_state': next_state
+        })
+    
+    def sample(self):
+        return np.random.choice(self.steps)
 
+class CumulativeRollout(Rollouts):
+    def __init__(self, discount_factor):
+        super().__init__()
+        self.discount_factor = discount_factor
+        
     def _compute_cumulative_rewards(self):
-        # Apply discounted accumulation
-        cumulative_rewards = self.rewards[:]
+        cumulative_rewards = [0] * len(self.steps)
         future_reward = 0
-        for i in reversed(range(len(cumulative_rewards))):
-            future_reward = cumulative_rewards[i] + future_reward * self.discount_factor
+        for i in reversed(range(len(self.steps))):
+            future_reward = self.steps[i]['reward'] + future_reward * self.discount_factor
             cumulative_rewards[i] = future_reward
-        # Normalize
         cumulative_rewards -= np.mean(cumulative_rewards)
         cumulative_rewards /= np.std(cumulative_rewards)
         return cumulative_rewards
-
-    # def close(self):
-    #     self.cumulative_rewards = self._compute_cumulative_rewards()
-    #
-    # def sample(self):
-    #     """
-    #         Returns sampled tuple (state, action, reward, next_state)
-    #         or (state, action, reward, next_state, cumulative_reward)
-    #     """
-    #     i = np.random.randint(len(self.states))
-    #     return self.states[i], self.actions[i], self.rewards[i], self.next_states[i]
-
+    
+    def close(self):
+        cumulative_rewards = self._compute_cumulative_rewards()
+        for i in range(len(self.steps)):
+            self.steps[i]['cumulative_reward'] = cumulative_rewards[i]
 
 class ACRollout(Rollouts):
-    """
-        Rollout which also stores a critic network's value predictions for the next state at each time step
-    """
-
     def __init__(self, discount_factor):
-        super().__init__(discount_factor)
+        super().__init__()
+        self.discount_factor = discount_factor
         self.end = []
 
-    def push(self, *args):
-        state, action, reward, s_, end = args
-        super().push(state, action, reward, s_)
-        self.end.append(end)
-
-    def sample(self):
-        """
-            Returns sampled tuple (state, action, reward, next_state, next_state_value_pred)
-        """
-        i = np.random.randint(len(self.states))
-        return self.states[i], self.actions[i], self.rewards[i], self.next_states[i], self.end[i]
+    def push(self, state, action, reward, next_state, end):
+        super().push(state, action, reward, next_state)
+        self.steps[-1]['end'] = end
 
 
 class ActorCritic:
     HIDDEN_SIZE_1 = 20
     HIDDEN_SIZE_2 = 20
-    LR = 0.00025
 
-    def __init__(self, state_dim, num_actions, discount_factor, tau):
+    def __init__(self, state_dim, num_actions, discount_factor=0.99, tau=0.01, learning_rate=0.00025):
+        self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.tau = tau
 
@@ -141,25 +121,24 @@ class ActorCritic:
         self.action_one_hot = tf.one_hot(self.action, num_actions)
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.action_one_hot, logits=self.actor_logits)
         self.actor_loss = tf.reduce_mean(cross_entropy * tf.stop_gradient(self.critic_advantage))
-        self.actor_op = tf.train.AdamOptimizer(learning_rate=self.LR).minimize(self.actor_loss)
+        self.actor_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.actor_loss)
 
-        self.critic_op = tf.train.AdamOptimizer(learning_rate=self.LR).minimize(self.critic_loss)
+        self.critic_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.critic_loss)
 
     def action_probs(self, state, session):
         feed_dict = {self.state: state}
         return session.run(self.actor_probs, feed_dict=feed_dict)
 
-    def update(self, rollout_sample, session):
-        state, action, reward, next_state, end = rollout_sample
-        critic_next_state_v = session.run(self.target_critic_state_V, feed_dict={self.state: [next_state]})
+    def update(self, rollout_step, session):
+        critic_next_state_v = session.run(self.target_critic_state_V, feed_dict={self.state: [rollout_step['next_state']]})
 
         feed_dict = {
-            self.state: [state],
-            self.action: action,
-            self.reward: reward,
-            self.next_state: [next_state],
+            self.state: [rollout_step['state']],
+            self.action: rollout_step['action'],
+            self.reward: rollout_step['reward'],
+            self.next_state: [rollout_step['next_state']],
             self.critic_next_state_V: critic_next_state_v,
-            self.end: end
+            self.end: rollout_step['end']
         }
         loss1, loss2, _, _ = session.run([self.actor_loss, self.critic_loss, self.actor_op, self.critic_op], feed_dict=feed_dict)
         return loss1, loss2
@@ -174,23 +153,24 @@ class ActorCritic:
     def update_target(self, sess):
         sess.run(self.update_target_params)
 
-
-def main():
+def lunar_lander(learning_rate, discount_factor, tau):
+    file_dir = "/Users/Praveen/Desktop/" + str(time.time_ns())
     with tf.Session() as sess:
         env = gym.make("LunarLander-v2")
-        env._max_episode_steps = 10000
-        agent = ActorCritic(state_dim=8, num_actions=4, discount_factor=0.99, tau=0.01) 
-
+        env._max_episode_steps = int(1E3)
+        agent = ActorCritic(state_dim=8, num_actions=4, discount_factor=0.99, tau=0.01, learning_rate=0.00025) 
         tf.global_variables_initializer().run()
         agent.init_target(sess)
         rollout = ACRollout(0.99)
         for i in range(25000):
             if i % 30 == 0:
-                rollout = ACRollout(0.99)
+                rollout.reset()
             total_reward = 0
             next_state_obs = env.reset()
             done = False
+            num_steps = 0
             while not done:
+                num_steps += 1
                 state_obs = next_state_obs
                 action = np.random.choice(np.arange(4), p=agent.action_probs(state_obs[np.newaxis, :], sess)[0])
                 next_state_obs, reward, done, _ = env.step(action)
@@ -202,7 +182,37 @@ def main():
             for j in range(100):
                 loss = agent.update(rollout.sample(), sess)
                 agent.update_target(sess)
-            print(f"Step {i}: Loss of {loss[0]}, {loss[1]}, reward of {total_reward}")
+            print(f"Step {i}: Loss of {loss[0]}, {loss[1]}, reward of {total_reward}. Took {num_steps} to end.")
+
+def main():
+    with tf.Session() as sess:
+        env = gym.make("LunarLander-v2")
+        env._max_episode_steps = int(1E3)
+        agent = ActorCritic(state_dim=8, num_actions=4, discount_factor=0.99, tau=0.01, learning_rate=0.00025) 
+        tf.global_variables_initializer().run()
+        agent.init_target(sess)
+        rollout = ACRollout(0.99)
+        for i in range(25000):
+            if i % 30 == 0:
+                rollout.reset()
+            total_reward = 0
+            next_state_obs = env.reset()
+            done = False
+            num_steps = 0
+            while not done:
+                num_steps += 1
+                state_obs = next_state_obs
+                action = np.random.choice(np.arange(4), p=agent.action_probs(state_obs[np.newaxis, :], sess)[0])
+                next_state_obs, reward, done, _ = env.step(action)
+                rollout.push(state_obs, action, reward, next_state_obs, int(done))
+                total_reward += reward
+                if i % 25 == 0:
+                    env.render()
+
+            for j in range(100):
+                loss = agent.update(rollout.sample(), sess)
+                agent.update_target(sess)
+            print(f"Step {i}: Loss of {loss[0]}, {loss[1]}, reward of {total_reward}. Took {num_steps} to end.")
 
 
 if __name__ == "__main__":
